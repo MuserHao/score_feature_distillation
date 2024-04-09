c57b8982ba19ac03c4ac597f8d6ce677ea383215
Hao Xin <hao9xin@gmail.com>
2024-04-08 23:35:42 -0500

    Eval class

--- eval_spair.py
+++ eval_spair.py
@@ -9,140 +9,147 @@ import os
 import json
 from PIL import Image
 import torch.nn as nn
-import matplotlib.pyplot as plt
 
-def get_data_lists(all_cats, json_list, testset_path):
-    cat2json = {}
-    cat2img = {}
-    for cat in all_cats:
-        cat2json[cat] = []
-        cat2img[cat] = []
-        for json_path in json_list:
-            if cat in json_path:
-                cat2json[cat].append(json_path)
-                with open(os.path.join(testset_path, json_path)) as temp_f:
+class SPairEvaluator:
+    def __init__(self, args):
+        self.args = args
+        for arg in vars(args):
+            value = getattr(args, arg)
+            if value is not None:
+                print('%s: %s' % (str(arg), str(value)))
+
+        self.dataset_path = args.dataset_path
+        self.test_path = 'PairAnnotation/test'
+        self.json_list = os.listdir(os.path.join(dataset_path, test_path))
+        self.all_cats = os.listdir(os.path.join(dataset_path, 'JPEGImages'))
+        self.save_path = args.save_path
+        self.img_size = args.img_size
+        self.t = args.t
+        self.up_ft_index = args.up_ft_index
+        self.ensemble_size = args.ensemble_size
+
+        if args.dift_model == 'sd':
+            self.dift = SDFeaturizer4Eval(cat_list=self.all_cats)
+        elif args.dift_model == 'adm':
+            self.dift = ADMFeaturizer4Eval()
+
+        self.cat2json = {}
+        self.cat2img = {}
+        # prepare json & image lists
+        self.get_data_lists()
+
+
+    def get_data_lists(self):
+        for cat in self.all_cats:
+            self.cat2json[cat] = []
+            self.cat2img[cat] = []
+            for json_path in self.json_list:
+                if cat in json_path:
+                    self.cat2json[cat].append(json_path)
+                    with open(os.path.join(self.dataset_path, self.test_path, self.json_path)) as temp_f:
+                        data = json.load(temp_f)
+                        temp_f.close()
+                    src_imname = data['src_imname']
+                    trg_imname = data['trg_imname']
+                    if src_imname not in self.cat2img[cat]:
+                        self.cat2img[cat].append(src_imname)
+                    if trg_imname not in self.cat2img[cat]:
+                        self.cat2img[cat].append(trg_imname)
+
+    def infer_and_save_features(self):
+        print("saving all test images' features...")
+        os.makedirs(self.save_path, exist_ok=True)
+        for cat in tqdm(self.all_cats):
+            output_dict = {}
+            for image_path in self.cat2img[cat]:
+                img = Image.open(os.path.join(self.all_cats, cat, image_path))
+                output_dict[image_path] = self.dift.forward(img,
+                                                    category=cat,
+                                                    img_size=self.img_size,
+                                                    t=self.t,
+                                                    up_ft_index=self.up_ft_index,
+                                                    ensemble_size=self.ensemble_size)
+            torch.save(output_dict, os.path.join(self.save_path, f'{cat}.pth'))
+            
+    def evaluate(self, verbal=False):
+        torch.cuda.set_device(0)
+        self.infer_and_save_features()
+        
+        total_pck = []
+        all_correct = 0
+        all_total = 0
+        pck_dict = {}
+        avg_pck_dict = {}
+
+        for cat in tqdm(all_cats):
+            output_dict = torch.load(os.path.join(self.args.save_path, f'{cat}.pth'))
+
+            cat_pck = []
+            cat_correct = 0
+            cat_total = 0
+
+            for json_path in self.cat2json[cat]:
+                with open(os.path.join(self.json_list, json_path)) as temp_f:
                     data = json.load(temp_f)
                     temp_f.close()
-                src_imname = data['src_imname']
-                trg_imname = data['trg_imname']
-                if src_imname not in cat2img[cat]:
-                    cat2img[cat].append(src_imname)
-                if trg_imname not in cat2img[cat]:
-                    cat2img[cat].append(trg_imname)
-    return cat2json, cat2img
 
-def infer_and_save_features(dift, all_cats, dataset_path, cat2img, args):
-    print("saving all test images' features...")
-    os.makedirs(args.save_path, exist_ok=True)
-    for cat in tqdm(all_cats):
-        output_dict = {}
-        for image_path in cat2img[cat]:
-            img = Image.open(os.path.join(dataset_path, 'JPEGImages', cat, image_path))
-            output_dict[image_path] = dift.forward(img,
-                                                category=cat,
-                                                img_size=args.img_size,
-                                                t=args.t,
-                                                up_ft_index=args.up_ft_index,
-                                                ensemble_size=args.ensemble_size)
-        torch.save(output_dict, os.path.join(args.save_path, f'{cat}.pth'))
-        
-def evaluation(args):
-    for arg in vars(args):
-        value = getattr(args,arg)
-        if value is not None:
-            print('%s: %s' % (str(arg),str(value)))
+                src_img_size = data['src_imsize'][:2][::-1]
+                trg_img_size = data['trg_imsize'][:2][::-1]
 
-    torch.cuda.set_device(0)
+                src_ft = output_dict[data['src_imname']]
+                trg_ft = output_dict[data['trg_imname']]
 
-    dataset_path = args.dataset_path
-    test_path = 'PairAnnotation/test'
-    json_list = os.listdir(os.path.join(dataset_path, test_path))
-    all_cats = os.listdir(os.path.join(dataset_path, 'JPEGImages'))
+                src_ft = nn.Upsample(size=src_img_size, mode='bilinear')(src_ft)
+                trg_ft = nn.Upsample(size=trg_img_size, mode='bilinear')(trg_ft)
+                h = trg_ft.shape[-2]
+                w = trg_ft.shape[-1]
 
-    # prepare json & image lists
-    cat2json, cat2img = get_data_lists(all_cats, json_list, os.path.join(dataset_path, test_path))
+                trg_bndbox = data['trg_bndbox']
+                threshold = max(trg_bndbox[3] - trg_bndbox[1], trg_bndbox[2] - trg_bndbox[0])
 
-    if args.dift_model == 'sd':
-        dift = SDFeaturizer4Eval(cat_list=all_cats)
-    elif args.dift_model == 'adm':
-        dift = ADMFeaturizer4Eval()
-    
-    infer_and_save_features(dift, all_cats, dataset_path, cat2img, args)
-    
-    total_pck = []
-    all_correct = 0
-    all_total = 0
-    cat_pck_dict = {}
-    cat_avg_pck_dict = {}
+                total = 0
+                correct = 0
 
-    for cat in all_cats:
-        output_dict = torch.load(os.path.join(args.save_path, f'{cat}.pth'))
+                for idx in range(len(data['src_kps'])):
+                    total += 1
+                    cat_total += 1
+                    all_total += 1
+                    src_point = data['src_kps'][idx]
+                    trg_point = data['trg_kps'][idx]
 
-        cat_pck = []
-        cat_correct = 0
-        cat_total = 0
+                    num_channel = src_ft.size(1)
+                    src_vec = src_ft[0, :, src_point[1], src_point[0]].view(1, num_channel) # 1, C
+                    trg_vec = trg_ft.view(num_channel, -1).transpose(0, 1) # HW, C
+                    src_vec = F.normalize(src_vec).transpose(0, 1) # c, 1
+                    trg_vec = F.normalize(trg_vec) # HW, c
+                    cos_map = torch.mm(trg_vec, src_vec).view(h, w).cpu().numpy() # H, W
 
-        for json_path in tqdm(cat2json[cat]):
-            with open(os.path.join(dataset_path, test_path, json_path)) as temp_f:
-                data = json.load(temp_f)
+                    max_yx = np.unravel_index(cos_map.argmax(), cos_map.shape)
 
-            src_img_size = data['src_imsize'][:2][::-1]
-            trg_img_size = data['trg_imsize'][:2][::-1]
+                    dist = ((max_yx[1] - trg_point[0]) ** 2 + (max_yx[0] - trg_point[1]) ** 2) ** 0.5
+                    if (dist / threshold) <= 0.1:
+                        correct += 1
+                        cat_correct += 1
+                        all_correct += 1
 
-            src_ft = output_dict[data['src_imname']]
-            trg_ft = output_dict[data['trg_imname']]
+                cat_pck.append(correct / total)
+            total_pck.extend(cat_pck)
 
-            src_ft = nn.Upsample(size=src_img_size, mode='bilinear')(src_ft)
-            trg_ft = nn.Upsample(size=trg_img_size, mode='bilinear')(trg_ft)
-            h = trg_ft.shape[-2]
-            w = trg_ft.shape[-1]
+            cat_avg_pck = np.mean(cat_pck) * 100 # average pck per image
+            cat_pck = cat_correct / cat_total * 100 # average pck
 
-            trg_bndbox = data['trg_bndbox']
-            threshold = max(trg_bndbox[3] - trg_bndbox[1], trg_bndbox[2] - trg_bndbox[0])
+            pck_dict[cat] = cat_pck
+            avg_pck_dict[cat] = cat_avg_pck
+            if verbal:
+                print(f'{cat} per image PCK@0.1: {cat_avg_pck:.2f}')
+                print(f'{cat} per point PCK@0.1: {cat_pck:.2f}')
 
-            total = 0
-            correct = 0
-
-            for idx in range(len(data['src_kps'])):
-                total += 1
-                cat_total += 1
-                all_total += 1
-                src_point = data['src_kps'][idx]
-                trg_point = data['trg_kps'][idx]
-
-                num_channel = src_ft.size(1)
-                src_vec = src_ft[0, :, src_point[1], src_point[0]].view(1, num_channel) # 1, C
-                trg_vec = trg_ft.view(num_channel, -1).transpose(0, 1) # HW, C
-                src_vec = F.normalize(src_vec).transpose(0, 1) # c, 1
-                trg_vec = F.normalize(trg_vec) # HW, c
-                cos_map = torch.mm(trg_vec, src_vec).view(h, w).cpu().numpy() # H, W
-
-                max_yx = np.unravel_index(cos_map.argmax(), cos_map.shape)
-
-                dist = ((max_yx[1] - trg_point[0]) ** 2 + (max_yx[0] - trg_point[1]) ** 2) ** 0.5
-                if (dist / threshold) <= 0.1:
-                    correct += 1
-                    cat_correct += 1
-                    all_correct += 1
-
-            cat_pck.append(correct / total)
-        total_pck.extend(cat_pck)
-
-        cat_avg_pck = np.mean(cat_pck) * 100 # average pck per image
-        cat_pck = cat_correct / cat_total * 100 # average pck
-
-        cat_pck_dict[cat] = cat_pck
-        cat_avg_pck_dict[cat] = cat_avg_pck
-        
-        
-        print(f'{cat} per image PCK@0.1: {cat_avg_pck:.2f}')
-        print(f'{cat} per point PCK@0.1: {cat_pck:.2f}')
-    
-    overall_avg_pck = np.mean(total_pck) * 100 # average pck per image
-    overall_pck = all_correct / all_total * 100 # overall pck
-    print(f'All per image PCK@0.1: {overall_avg_pck:.2f}')
-    print(f'All per point PCK@0.1: {overall_pck:.2f}')
-    return overall_pck, overall_avg_pck, cat_pck_dict, cat_avg_pck_dict
+        pck_dict['overall'] = all_correct / all_total * 100 # overall pck
+        avg_pck_dict['overall'] = np.mean(total_pck) * 100 # average pck per image
+        if verbal:
+            print(f'All per image PCK@0.1: {avg_pck_dict['overall']:.2f}')
+            print(f'All per point PCK@0.1: {pck_dict['overall']:.2f}')
+        return pck_dict, avg_pck_dict
 
 
 if __name__ == "__main__":
@@ -158,4 +165,5 @@ if __name__ == "__main__":
     parser.add_argument('--up_ft_index', default=1, type=int, help='which upsampling block to extract the ft map')
     parser.add_argument('--ensemble_size', default=8, type=int, help='ensemble size for getting an image ft map')
     args = parser.parse_args()
-    evaluation(args)
+    evaluator = SPairEvaluator(args)
+    evaluator.evaluate()
