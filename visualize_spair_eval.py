1275ab8c287a67b693195a56ae18a9609854c0c4
Hao Xin <hao9xin@gmail.com>
2024-04-09 00:15:21 -0500

    Second version for test

--- visualize_spair_eval.py
+++ visualize_spair_eval.py
@@ -1,28 +1,30 @@
 import argparse
 import matplotlib.pyplot as plt
-from SpairEvaluator import SpairEvaluator
+from eval_spair import SpairEvaluator
 
 def main(args):
     # Initialize the SpairEvaluator
-    evaluator = SpairEvaluator(args.dataset_path, args.save_path, args.dift_model, args.img_size, args.up_ft_index, args.ensemble_size)
+    args.t = 0
+    evaluator = SpairEvaluator(args)
 
     # Define the range of 't' values to evaluate
-    t_values = range(0, 1001, 100)
+    t_values = range(0, 1001, 20)
 
     # Initialize lists to store evaluation results
     per_image_pck = []
-    per_point_pck = []
+    pck = []
 
     for t in t_values:
-        args.t = t
+        evaluator.set_t(t)
         # Evaluate using the SpairEvaluator
-        per_image_pck.append(evaluator.evaluate_per_image_pck())
-        per_point_pck.append(evaluator.evaluate_per_point_pck())
+        metric_dict, per_image_metric_dict = evaluator.evaluate()
+        per_image_pck.append(metric_dict['overall'])
+        pck.append(per_image_metric_dict['overall'])
 
     # Plot and save the evaluation curve
     plt.figure()
     plt.plot(t_values, per_image_pck, label='Per Image PCK@0.1')
-    plt.plot(t_values, per_point_pck, label='Per Point PCK@0.1')
+    plt.plot(t_values, pck, label='Per Point PCK@0.1')
     plt.xlabel('t')
     plt.ylabel('PCK')
     plt.title('Evaluation Metric vs. t')
@@ -30,7 +32,7 @@ def main(args):
     plt.savefig('evaluation_curve.png')
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='SPair-71k Evaluation Script')
+    parser = argparse.ArgumentParser(description='SPair-71k Evaluation Visualize Script')
     parser.add_argument('--dataset_path', type=str, default='./SPair-71k/', help='path to spair dataset')
     parser.add_argument('--save_path', type=str, default='/scratch/lt453/spair_ft/', help='path to save features')
     parser.add_argument('--dift_model', choices=['sd', 'adm'], default='sd', help="which dift version to use")
@@ -38,7 +40,7 @@ if __name__ == "__main__":
                         help='''in the order of [width, height], resize input image
                             to [w, h] before fed into diffusion model, if set to 0, will
                             stick to the original input size. by default is 768x768.''')
-    parser.add_argument('--t', default=261, type=int, help='t for diffusion')
+    parser.add_argument('--t_range', nargs='+', type=int, default=[0, 1001] help='range of t for diffusion')
     parser.add_argument('--up_ft_index', default=1, type=int, help='which upsampling block to extract the ft map')
     parser.add_argument('--ensemble_size', default=8, type=int, help='ensemble size for getting an image ft map')
     args = parser.parse_args()
