040cf4185f3da87a985d76b8c3fccef444c8248f
Hao Xin <hao9xin@gmail.com>
2024-04-17 16:52:01 -0500

    Update eval_spair.py

--- eval_spair.py
+++ eval_spair.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='SPair-71k Evaluation Script')
     parser.add_argument('--dataset_path', type=str, default='./SPair-71k/', help='path to spair dataset')
     parser.add_argument('--save_path', type=str, default='/scratch/lt453/spair_ft/', help='path to save features')
-    parser.add_argument('--dift_model', choices=['sd', 'adm'], default='sd', help="which dift version to use")
+    parser.add_argument('--dift_model', choices=['sd', 'adm', 'sdh'], default='sd', help="which dift version to use")
     parser.add_argument('--img_size', nargs='+', type=int, default=[768, 768],
                         help='''in the order of [width, height], resize input image
                             to [w, h] before fed into diffusion model, if set to 0, will
@@ -170,7 +170,7 @@ if __name__ == "__main__":
     parser.add_argument('--t', nargs='+', default=[261], type=int, help='t list for diffusion')
     parser.add_argument('--cat_list', nargs='+', default=[], type=str, help='Category list to process')
     parser.add_argument('--up_ft_index', default=1, type=int, help='which upsampling block to extract the ft map')
-    parser.add_argument('--ensemble_size', default=8, type=int, help='ensemble size for getting an image ft map')
+    parser.add_argument('--ensemble_size', default=4, type=int, help='ensemble size for getting an image ft map')
     args = parser.parse_args()
     evaluator = SPairEvaluator(args)
-    evaluator.evaluate()
+    evaluator.evaluate(vocal=True)
