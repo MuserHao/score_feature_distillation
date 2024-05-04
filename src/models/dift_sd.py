97a964d6967b3f274dfcdd1aad0864bf0488b203
Hao Xin <hao9xin@gmail.com>
2024-05-04 15:55:04 -0500

    Update dift_sd.py

--- src/models/dift_sd.py
+++ src/models/dift_sd.py
@@ -290,3 +290,59 @@ class SDFeaturizer4Eval(SDFeaturizer):
         unet_ft = unet_ft_all['up_ft'][up_ft_index] # ensem, c, h, w
         unet_ft = unet_ft.mean(0, keepdim=True) # 1,c,h,w
         return unet_ft
+
+class SD_Gradient_Featurizer4Eval(SDFeaturizer):
+    def __init__(self, sd_id='stabilityai/stable-diffusion-2-1', null_prompt='', cat_list=[]):
+        super().__init__(sd_id, null_prompt)
+        with torch.no_grad():
+            cat2prompt_embeds = {}
+            for cat in cat_list:
+                prompt = f"a photo of a {cat}"
+                prompt_embeds = self.pipe._encode_prompt(
+                    prompt=prompt,
+                    device='cuda',
+                    num_images_per_prompt=1,
+                    do_classifier_free_guidance=False) # [1, 77, dim]
+                cat2prompt_embeds[cat] = prompt_embeds
+            self.cat2prompt_embeds = cat2prompt_embeds
+
+        self.pipe.tokenizer = None
+        self.pipe.text_encoder = None
+        gc.collect()
+        torch.cuda.empty_cache()
+
+    @torch.no_grad()
+    def forward(self,
+                img,
+                category=None,
+                img_size=[768, 768],
+                t=261,
+                up_ft_index=1,
+                ensemble_size=8):
+        if img_size is not None:
+            img = img.resize(img_size)
+        img_tensor = (PILToTensor()(img) / 255.0 - 0.5) * 2
+        img_tensor = img_tensor.unsqueeze(0).repeat(ensemble_size, 1, 1, 1).cuda() # ensem, c, h, w
+        if category in self.cat2prompt_embeds:
+            prompt_embeds = self.cat2prompt_embeds[category]
+        else:
+            prompt_embeds = self.null_prompt_embeds
+        prompt_embeds = prompt_embeds.repeat(ensemble_size, 1, 1).cuda()
+
+        img_tensor.requires_grad = True  # Enable gradient computation
+
+        unet_ft_all = self.pipe(
+            img_tensor=img_tensor,
+            t=t,
+            up_ft_indices=[up_ft_index],
+            prompt_embeds=prompt_embeds)
+        unet_ft = unet_ft_all['up_ft'][up_ft_index] # ensem, c, h, w
+        unet_ft = unet_ft.mean(0, keepdim=True) # 1,c,h,w
+
+        # Calculate gradient of feature map with respect to input image
+        unet_ft.backward(torch.ones_like(unet_ft))  
+
+        # Retrieve the gradients
+        gradients = img_tensor.grad
+
+        return gradients
